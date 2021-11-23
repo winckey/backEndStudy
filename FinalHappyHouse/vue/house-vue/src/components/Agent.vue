3ffea1f4c9ebad918623b4e2f2d7da8b4065dee2
isaac <isaac1450@gnu.ac.kr>
2021-11-23 22:10:05 +0900

    sac 1123 10:10

--- FinalHappyHouse/vue/house-vue/src/components/Agent.vue
+++ FinalHappyHouse/vue/house-vue/src/components/Agent.vue
@@ -17,45 +17,47 @@
                   <li class="breadcrumb-item">
                     <a href="#">Home</a>
                   </li>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    Agents Grid
-                  </li>
+                  <li class="breadcrumb-item active" aria-current="page">Agents Grid</li>
                 </ol>
               </nav>
             </div>
           </div>
         </div>
-      </section><!-- End Intro Single-->
+      </section>
+      <!-- End Intro Single-->
 
       <!-- ======= Agents Grid ======= -->
       <section class="agents-grid grid">
         <div class="container">
           <div class="row">
             <!-- agent -->
-            <div class="col-md-4" v-for="(agent, index) in this.$store.state.agent.list" v-bind:key="index">
+            <div
+              class="col-md-4"
+              v-for="(agent, index) in this.$store.state.agent.list"
+              v-bind:key="index"
+            >
               <div class="card-box-d">
                 <div class="card-img-d">
-                  <img src="assets/img/agent-4.jpg" alt="" class="img-d img-fluid">
+                  <img src="assets/img/agent-4.jpg" alt="" class="img-d img-fluid" />
                 </div>
                 <div class="card-overlay card-overlay-hover">
+                  <button type="button" class="btn btn-warning justify-contents-end">
+                    <i class="bi bi-star me-1"></i>
+                  </button>
                   <div class="card-header-d">
                     <div class="card-title-d align-self-center">
                       <h3 class="title-d">
-                        <div @click="agentDetail(agent.agentNo)">{{agent.agentName}}</div>
+                        <div @click="agentDetail(agent.agentNo)">{{ agent.agentName }}</div>
                       </h3>
                     </div>
                   </div>
                   <div class="card-body-d">
                     <p class="content-d color-text-a">
-                      {{agent.agentDesc}}
+                      {{ agent.agentDesc }}
                     </p>
                     <div class="info-agents color-a">
-                      <p>
-                        <strong>Phone: </strong>{{agent.agentPhone}}
-                      </p>
-                      <p>
-                        <strong>Email: </strong> {{agent.agentEmail}}
-                      </p>
+                      <p><strong>Phone: </strong>{{ agent.agentPhone }}</p>
+                      <p><strong>Email: </strong> {{ agent.agentEmail }}</p>
                     </div>
                   </div>
                   <div class="card-footer-d">
@@ -88,32 +90,27 @@
               </div>
             </div>
             <!-- agent -->
-
           </div>
         </div>
       </section>
-
     </main>
-
   </div>
 </template>
 
 <script>
-  export default {
-
-    methods: {
-      agentList() {
-        this.$store.dispatch('agentList');
-      },
-      agentDetail(agentNo) {
-        this.$store.commit('SET_AGENT_NO', agentNo);
-        this.$router.push("/agentDetail");
-      },
+export default {
+  methods: {
+    agentList() {
+      this.$store.dispatch("agentList");
     },
-
-
-    created() {
-      this.agentList();
+    agentDetail(agentNo) {
+      this.$store.commit("SET_AGENT_NO", agentNo);
+      this.$router.push("/agentDetail");
     },
-  };
+  },
+
+  created() {
+    this.agentList();
+  },
+};
 </script>
